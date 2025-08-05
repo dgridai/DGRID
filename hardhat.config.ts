b41eb74072836c4b3e5a8c19221a50af9ebfe37f
dgriddev <dgriddev@outlook.com>
2025-08-05 14:11:12 +0800

    chore : readme

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-verify'
+import '@openzeppelin/hardhat-upgrades';
+import "hardhat-deploy"
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    },
+  },
+  sourcify: {
+    enabled: true
+  }
 };
 
 export default config;
