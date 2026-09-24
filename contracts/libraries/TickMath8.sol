f19a47de23864ee717440594e0a16f4cb2a98522
zk-xl <qkl35428@gmial.com>
2026-09-24 16:26:39 +0800

    audit: fixed TickMath MIN_SQRT_RATIO and MAX_SQRT_RATIO

--- contracts/libraries/TickMath8.sol
+++ contracts/libraries/TickMath8.sol
@@ -5,9 +5,9 @@ library TickMath {
     int24 internal constant MIN_TICK = -887272;
     int24 internal constant MAX_TICK = -MIN_TICK;
 
-    uint160 internal constant MIN_SQRT_RATIO = 4295128739 + 1;
+    uint160 internal constant MIN_SQRT_RATIO = 4295128739;
     uint160 internal constant MAX_SQRT_RATIO =
-        1461446703485210103287273052203988822378723970342 - 1;
+        1461446703485210103287273052203988822378723970342;
 
     // get sqrtPriceX96 for a given tick
     function getSqrtRatioAtTick(
